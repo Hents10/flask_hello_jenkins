18d360b815374d9af6a9f579131fafaa5ed1796a
Hents10 <henintsoamahafaly@gmail.com>
2026-05-20 12:52:09 +0300

    Ajout failing TDD test for feature route

--- test.py
+++ test.py
@@ -20,10 +20,15 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_name(self):
-        name = 'Simon'
+        name = 'Henintsoa'
         rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
+
+    def test_new_route(self):
+        name = "test"
+        rv = self.app.get(f'/feature/{name}')
+        self.assertEqual(rv.status, '200 OK')
 
 if __name__ == '__main__':
     unittest.main()
